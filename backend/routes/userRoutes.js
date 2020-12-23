cdd52bd01df1056213e69241416f12561f5d6f31
Charlie <charlyfinos@gmail.com>
2020-12-23 09:40:43 -0300

    Update user details endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,15 @@ import {
     authUser,
     registerUser,
     getUsers,
-    getUserProfile
+    getUserProfile,
+    updateUserProfile
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
 router.route('/signup').post(registerUser)
 router.route('/').get(getUsers)
 
