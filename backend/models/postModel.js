841c781e62cc5b8b7f707938c8b296b3719c6cb4
Charlie <charlyfinos@gmail.com>
2021-01-18 17:47:33 -0300

    Change on postModel and Delete commentModel

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -1,5 +1,19 @@
 import mongoose from 'mongoose'
 
+const commentSchema = mongoose.Schema({
+    username: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
 const postSchema = mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
@@ -13,10 +27,7 @@ const postSchema = mongoose.Schema({
         type: String,
         required: true,
     },
-    comments: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Comment'
-    }]
+    comments: [commentSchema]
 }, {
     timestamps: true,
 })
