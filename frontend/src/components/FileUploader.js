74eb235522cfda1d3b811d95141808cc5f5fb45b
Charlie <charlyfinos@gmail.com>
2021-01-28 18:54:19 -0300

    Set Modal Component

--- frontend/src/components/FileUploader.js
+++ frontend/src/components/FileUploader.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './FileUploader.css'
+import Modal from 'react-modal'
 
 const FileUploader = () => {
     const [modal, setModal] = useState(false)
@@ -8,10 +9,18 @@ const FileUploader = () => {
         setModal(!modal)
     }
 
+    const closeModal = () => {
+        setModal(false)
+    }
     return (
         <div className='file-uploader'>
             <button onClick={modalHandler}>New Post</button>
-            {modal ? <h3>modal</h3> : null}
+            <Modal isOpen={modal}>
+                <div className='file-uploader__close__button'>
+                    <button onClick={closeModal}>x</button>
+                </div>
+                <h2>Modal</h2>
+            </Modal>
         </div>
     )
 }
