9e7f37251a83b1b83a37ef6eaf29701afc144697
Charlie <charlyfinos@gmail.com>
2021-01-29 18:56:45 -0300

    Styling file-uploader form

--- frontend/src/components/FileUploader.js
+++ frontend/src/components/FileUploader.js
@@ -4,6 +4,18 @@ import Modal from 'react-modal'
 
 const FileUploader = () => {
     const [modal, setModal] = useState(false)
+    const [caption, setCaption] = useState('')
+
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)'
+        }
+    }
 
     const modalHandler = () => {
         setModal(!modal)
@@ -12,14 +24,30 @@ const FileUploader = () => {
     const closeModal = () => {
         setModal(false)
     }
+
+    const uploadFileHandler = (e) => {
+        e.preventDefault()
+        console.log(caption)
+    }
+
     return (
         <div className='file-uploader'>
             <button onClick={modalHandler}>New Post</button>
-            <Modal isOpen={modal}>
+            <Modal
+                ariaHideApp={false}
+                style={customStyles}
+                isOpen={modal}>
                 <div className='file-uploader__close__button'>
                     <button onClick={closeModal}>x</button>
                 </div>
-                <h2>Modal</h2>
+                <div className='file-uploader__form'>
+                    <form onSubmit={uploadFileHandler}>
+                        <label name='text'>Caption</label>
+                        <input type='text' placeholder='Put a caption here...' value={caption} onChange={e => setCaption(e.target.value)}></input>
+
+                        <button type='submit'>Submit</button>
+                    </form>
+                </div>
             </Modal>
         </div>
     )
