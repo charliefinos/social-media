722a8230ad954f0734dbaaee6100797cadffe515
Charlie <charlyfinos@gmail.com>
2021-02-08 17:50:53 -0300

    Fix create comment route

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 import {
     createPost,
+    createPostComment,
     getPost,
     getUserPosts,
 } from '../controllers/postController.js'
@@ -10,6 +11,7 @@ import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, createPost)
 router.route('/profile').get(protect, getUserPosts)
+router.route('/:id/comments').post(protect, createPostComment)
 router.route('/:id').get(protect, getPost)
 
 export default router
