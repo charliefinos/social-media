ae43a179c110c1d57195986a6132e1931776e1e7
Charlie <charlyfinos@gmail.com>
2021-03-22 10:06:36 -0300

    add profileImg to user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -21,6 +21,9 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    profileImg: {
+        type: String,
+    },
     city: {
         type: String
     },
