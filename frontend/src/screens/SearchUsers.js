1b33dab7598452a4868c284e9d5d9d22d5e83697
Charlie <charlyfinos@gmail.com>
2021-03-29 13:24:30 -0300

    search users list template

--- frontend/src/screens/SearchUsers.js
+++ frontend/src/screens/SearchUsers.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Container, Col, Row, Image } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const SearchUsers = () => {
     return (
-        <div>
-            User Search
-        </div>
+        <Container>
+            <Row>
+                <Col lg={2} md={3} sm={3} xs={3}>
+                    <Image fluid roundedCircle src='https://blog.liquid.com/hubfs/vitalik.jpg' />
+                </Col>
+                <Col className="d-flex flex-column" lg={10} md={9} sm={9} xs={9}>
+                    <Link to='/charliefinos'>charliefinos</Link>
+                    <a><strong>Followers: </strong>2323</a>
+                    <a><strong>Following: </strong></a>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
