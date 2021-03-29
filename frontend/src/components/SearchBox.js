2d3756326b51ed1ae0cde1f0a420945f482192f3
Charlie <charlyfinos@gmail.com>
2021-03-29 13:57:35 -0300

    add trim to keyword

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -7,7 +7,7 @@ const SearchBox = ({ history }) => {
     const submitHandler = (e) => {
         e.preventDefault()
         if (keyword.trim()) {
-            history.push(`/search/${keyword}`)
+            history.push(`/search/${keyword.trim()}`)
         } else {
             history.push('/')
         }
