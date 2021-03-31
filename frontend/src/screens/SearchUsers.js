d4bca0ee7b46b966cec19f7d4f6e72664d818389
Charlie <charlyfinos@gmail.com>
2021-03-31 17:12:45 -0300

    adding a add friend button

--- frontend/src/screens/SearchUsers.js
+++ frontend/src/screens/SearchUsers.js
@@ -20,21 +20,24 @@ const SearchUsers = ({ match }) => {
         <Container>
             {loading && <h1>Loading</h1>}
             {success && (userSearch.length === 0 ? (
-                <h1>No post found!</h1>
+                <h1>No users found!</h1>
             ) :
                 (userSearch.map((user) => (
                     <Row className='my-2 py-1 border-bottom' key={user._id}>
                         <Col className='mr-3' xs={4} md={3} lg={3}>
                             <Image fluid roundedCircle src={user.profileImg}></Image>
                         </Col>
+
                         <Col xs={4} md={4} lg={4}>
                             <p><strong>{user.name}</strong></p>
                             <Link to={`/${user.username}`}><p>@{user.username}</p></Link>
                             <p><strong>Email: </strong>{user.email}</p>
                         </Col>
+
                         <Col className='mt-4' xs={3} md={3} lg={3}>
                             <Button variant='secondary'>Add friend</Button>
                         </Col>
+
                         <br />
                     </Row>
                 ))))}
