c4a396bb64666bf097a48fd9305f457da1a63ad1
Charlie <charlyfinos@gmail.com>
2021-04-13 16:50:21 -0300

    styling on profile component

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -5,24 +5,29 @@ import { Button, Image, Row, Col } from 'react-bootstrap'
 const Profile = ({ user }) => {
 
     return (
-        <Row className="d-flex" data-test="d-flex" >
-            <Col xs={5} >
+        <Row className="mb-3  w-75 d-inline-flex border" >
+            <Col className="w-25" md={4} xs={4} >
                 <Image
+                    className="my-5 rounded"
                     fluid
                     src={user.profileImg}
                 ></Image>
 
             </Col>
-            <Col xs={7} >
-                <h3>{user.name}</h3>
-                <br />
-                <p>@{user.username}</p>
-                <p><strong>Contact:</strong> {user.email}</p>
+            <Col className="w-50 py-4" md={5} xs={5} >
+                <h3 className="mb-0">{user.name}</h3>
+                <p className="text-muted">@{user.username}</p>
+
+                <p className="mb-1"><strong>Followers:</strong> 456</p>
+                <p className="mb-1"><strong>Contact:</strong> {user.email}</p>
                 {user.bio && <p><strong>Bio:</strong> {user.bio}</p>}
-                <br />
+
+            </Col>
+            <Col className="w-25" md={3} xs={3}>
                 <LinkContainer
+                    className="mt-5"
                     to="/profile/edit"
-                ><Button className="mx-2" variant="dark">Edit</Button></LinkContainer>
+                ><Button variant="danger">Edit</Button></LinkContainer>
             </Col>
         </Row>
     )
