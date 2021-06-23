7f3221d2bcdd4f69eaf7f5a21a8c56f098a4d6f7
Charlie <charlyfinos@gmail.com>
2021-06-23 16:53:46 -0300

    change style on header and post components

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
@@ -6,10 +6,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../actions/UserActions";
 import { Button, Navbar, DropdownButton, Dropdown } from "react-bootstrap";
 import SearchBox from "./SearchBox";
+import './Header.scss'
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const dispatch = useDispatch();
     const history = useHistory();
+
+    const [toggle, setToggle] = useState(false)
 
     const userLogin = useSelector((state) => state.userLogin);
     const { userInfo } = userLogin;
@@ -19,39 +23,45 @@ const Header = () => {
         history.push("/account/login");
     };
 
-    return (
-        <Navbar
-            className="d-flex justify-content-between py-3 navbar"
-            bg="secondary"
-            variant="dark"
-        >
-            <Navbar.Brand href="/">Social-Media</Navbar.Brand>
+    const dropdownToggle = () => {
+        setToggle(!toggle)
+    }
 
-            {userInfo ? (
-                <>
-                    <Route render={({ history }) => <SearchBox history={history} />} />
-                    <DropdownButton
-                        variant="dark"
-                        id="dropdown-basic-button"
-                        title={userInfo.name}
-                    >
-                        <Dropdown.Item href={`/${userInfo.username}`}>
-                            Profile
-                        </Dropdown.Item>
-                        <Dropdown.Item onClick={logoutHandler}>Logout</Dropdown.Item>
-                    </DropdownButton>
-                </>
-            ) : (
-                <div>
-                    <LinkContainer to="/account/signup">
-                        <Button variant="primary">Signup</Button>
-                    </LinkContainer>
-                    <LinkContainer to="/account/login">
-                        <Button variant="primary">Login</Button>
-                    </LinkContainer>
+    return (
+        <>
+            <div className="header">
+                <div className="header__logo">
+                    <h1>Logo</h1>
                 </div>
-            )}
-        </Navbar>
+                {userInfo ? (
+                    <>
+
+                        <div className="header__searchbar">
+                            <SearchBox />
+                        </div>
+
+                        <div className="header__right">
+                            <p>{userInfo.username}</p>
+                            <div class="dropdown" >
+                                <img onClick={dropdownToggle} src={userInfo.profileImg}></img>
+                                <div class={`dropdown-content ${toggle ? 'display' : ''}`}>
+                                    <a onClick={dropdownToggle} to={`/${userInfo.username}`}>Profile</a>
+                                    <a onClick={logoutHandler}>Logout</a>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <div className="header__right__out">
+                        <Link className="btn-1" to={`/account/login`}>Login</Link>
+                        <Link className="btn-2" to={`/account/signup`}>Signup</Link>
+                    </div>
+                )}
+
+
+            </div>
+
+        </>
     );
 };
 
